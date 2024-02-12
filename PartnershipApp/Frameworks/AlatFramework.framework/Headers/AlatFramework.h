13689586eb55233a9d883ca03f990275af4b9a96
= <abdullahijimoh3.ja@gmail.com>
2024-02-12 14:01:30 +0100

    first commit

---
+++ PartnershipApp/Frameworks/AlatFramework.framework/Headers/AlatFramework.h
@@ -0,0 +1,21 @@
+//
+//  AlatFramework.h
+//  AlatFramework
+//
+//  Created by Adjogbe  Tejiri on 02/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "iCarousel.h"
+
+//! Project version number for AlatFramework.
+FOUNDATION_EXPORT double AlatFrameworkVersionNumber;
+
+//! Project version string for AlatFramework.
+FOUNDATION_EXPORT const unsigned char AlatFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlatFramework/PublicHeader.h>
+
+
+
